798aaff0193e0759a7f3949935c355161eb100f1
A.Seledtsov <49916559+VitoBravis@users.noreply.github.com>
2020-02-14 16:23:48 +0700

    Sticky Header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 /*JQuery не использовать*/
+
+let logo = document.getElementsByClassName("logo")[0];
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", scrollHeader);
+function scrollHeader() {
+    let windowHeight = document.documentElement.clientHeight;
+    if (window.pageYOffset > windowHeight && !header.classList.contains("header--sticky")){
+        header.classList.toggle("header--sticky");
+        logo.src = "images/logo2.svg";
+    } else if(window.pageYOffset <= windowHeight && header.classList.contains("header--sticky")){
+        header.classList.toggle("header--sticky");
+        logo.src = "images/logo1.svg";
+    }
+
+}
