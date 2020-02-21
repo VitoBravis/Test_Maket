2ca5c7246d1cf25edd54aed0506afd52703d6cce
A.Seledtsov <49916559+VitoBravis@users.noreply.github.com>
2020-02-22 02:36:40 +0700

    scroll animations

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 /*JQuery не использовать*/
 
-var swiperNews = new Swiper('.swiper-container--news', {
+const swiperNews = new Swiper('.swiper-container--news', {
     slidesPerView: 3,
     // freeMode: true,
     spaceBetween: 30,
     slidesOffsetBefore: document.documentElement.clientWidth * 0.156,
 });
 
-var swiperIndex = new Swiper('.swiper-container--index', {
+const swiperIndex = new Swiper('.swiper-container--index', {
     direction: 'horizontal',
     loop: true,
     effect: 'fade',
@@ -22,11 +22,17 @@ var swiperIndex = new Swiper('.swiper-container--index', {
     },
 });
 
-let logo = document.getElementsByClassName("logo")[0];
 let header = document.querySelector("header");
+let logo = document.querySelector(".logo");
+let infoItem1 = document.getElementsByClassName("info__item--1");
+let infoItem2 = document.getElementsByClassName("info__item--2");
+let aboutInner = document.getElementsByClassName("about__inner");
+let worksTitles = document.getElementsByClassName("works__title");
+let mapItems = document.getElementsByClassName("map__item");
+let newsInner = document.getElementsByClassName("news__inner");
 
-window.addEventListener("scroll", scrollHeader);
-function scrollHeader() {
+window.addEventListener("scroll", scrollEvent);
+function scrollEvent() {
     let windowHeight = document.documentElement.clientHeight;
     if (window.pageYOffset > windowHeight && !header.classList.contains("header--sticky")){
         header.classList.toggle("header--sticky");
@@ -34,5 +40,19 @@ function scrollHeader() {
     } else if(window.pageYOffset <= windowHeight && header.classList.contains("header--sticky")){
         header.classList.toggle("header--sticky");
         logo.src = "images/logo1.svg";
+    }
+    scrollAnimation(aboutInner, "_slide-right");
+    scrollAnimation(worksTitles, "_slide-up");
+    scrollAnimation(mapItems, "_fade-in");
+    scrollAnimation(newsInner, "_slide-left");
+    scrollAnimation(infoItem1, "_slide-right");
+    scrollAnimation(infoItem2, "_slide-left");
+
+    function scrollAnimation(itemList, anim) {
+        for(let i=0; i<itemList.length; i++) {
+            if (itemList[i].getBoundingClientRect().y <= windowHeight && !itemList[i].classList.contains(anim)) {
+                itemList[i].classList.toggle(anim);
+            }
+        }
     }
 }
